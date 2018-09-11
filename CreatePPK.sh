74a5b256a48d3a7d27e0c69b87cf88f9fdce2ac5
ChulHyun, Baek <chbaek@outlook.com>
2018-09-11 16:42:54 +0900

    Update CreatePPK.sh

--- CreatePPK.sh
+++ CreatePPK.sh
@@ -1,8 +1,7 @@
 # -- Jul 14, 2017 -- # 
-# -- chb @ mz.co.kr -- # 
+# -- liverpools@gmail.com -- # 
 
 # puttygen input.pem  -O private -o output.ppk
 input=$1
 
 puttygen $input.pem  -O private -o $input.ppk
-
