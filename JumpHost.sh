d021066264204c9d149449e94f7706d544233ef6
ChulHyun, Baek <chbaek@outlook.com>
2018-09-19 08:43:34 +0900

    Update JumpHost.sh

--- JumpHost.sh
+++ JumpHost.sh
@@ -1,5 +1,7 @@
 #!/bin/sh 
 
+# -- By liverpools@gmail.com -- # 
+# -- On someday -- # 
 # -- If you use jump host in aws, you can use this command in mac os -- # 
 
 ssh -i my.pem -tt ec2-user@ip_address_of_jump_host ssh -i my.pem ec2-user@ip_address_of_internal 
